4f65ec85b549fa70c51e85fcb9a58c913413f81d
waqasahmed94 <66775969+waqasahmed94@users.noreply.github.com>
2020-08-21 09:06:40 +0000

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Demo from './demo'
+
+function App() {
+  return (
+    <div className="App">
+          <h1>Hello Guys.......!</h1>
+          <Demo name= "Props" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/demo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Demo (props) {
+    return (
+        <h1>This is example of {props.name} data</h1>
+    );
+}
+
+export default Demo;
